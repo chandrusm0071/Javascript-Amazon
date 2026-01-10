6411f42eee4d1caf235d9e9e52b2995f8bd7727f
Chandru37-ctrl <smchandru37@gmail.com>
2026-01-10 15:57:29 +0530

    corrected the delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 
 let cartSummaryHTML ='';
@@ -12,7 +12,8 @@ cart.forEach((item) =>{
     });
     
     cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container
+        js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -35,7 +36,7 @@ cart.forEach((item) =>{
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary">
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                 Delete
                 </span>
             </div>
@@ -48,7 +49,7 @@ cart.forEach((item) =>{
             <div class="delivery-option">
                 <input type="radio" checked
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Tuesday, June 21
@@ -61,7 +62,7 @@ cart.forEach((item) =>{
             <div class="delivery-option">
                 <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Wednesday, June 15
@@ -88,5 +89,17 @@ cart.forEach((item) =>{
         </div>
         </div>
     `;
-    document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML
-})
+});
+document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+document.querySelectorAll('.js-delete-link')
+    .forEach((link)=>{
+        link.addEventListener('click',()=>{
+            const productId = link.dataset.productId;
+            removeFromCart(productId);
+
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
+        });
+    });
+
+
